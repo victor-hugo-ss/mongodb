935d3f9fcd5173e7823d3951190bec18fdb88872
victor-hugo-ss <victorabreu2512@gmail.com>
2026-04-01 12:13:50 -0300

    Funcionalidade de cadastro de produtos no mongoDB

---
+++ 3_inserindo_dados/controllers/ProductController.js
@@ -0,0 +1,21 @@
+import Product from "../models/Product.js";
+
+export default class ProductController {
+  static showProducts(req, res) {
+    res.render("products/all");
+  }
+
+  static createProduct(req, res) {
+    res.render("products/create");
+  }
+
+  static createProductPost(req, res) {
+    const { name, price, description } = req.body;
+
+    const product = new Product(name, price, description);
+
+    product.save();
+
+    res.redirect("/products");
+  }
+}
