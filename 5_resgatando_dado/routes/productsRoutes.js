a6dead2d9ce6e0902c97f50bfd3260f8b517962f
victor-hugo-ss <victorabreu2512@gmail.com>
2026-04-02 08:35:15 -0300

    Funcionalidade de resgate de produto

---
+++ 5_resgatando_dado/routes/productsRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+
+import ProductsController from "../controllers/ProductController.js";
+
+router.get("/create", ProductsController.createProduct);
+router.post("/create", ProductsController.createProductPost);
+router.get("/:id", ProductsController.getProduct);
+router.get("/", ProductsController.showProducts);
+
+export default router;
