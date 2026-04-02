e23807a067809e6d280fe3af66dd8d7333c63429
victor-hugo-ss <victorabreu2512@gmail.com>
2026-04-02 10:30:58 -0300

    Funcinalidade de edição

---
+++ 7_editando_dados/routes/productsRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+
+import ProductsController from "../controllers/ProductController.js";
+
+router.get("/create", ProductsController.createProduct);
+router.post("/create", ProductsController.createProductPost);
+router.post("/remove/:id", ProductsController.removeProduct);
+router.get("/edit/:id", ProductsController.editProduct);
+router.get("/:id", ProductsController.getProduct);
+router.get("/", ProductsController.showProducts);
+
+export default router;
